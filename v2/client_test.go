b18d1d866dc1eadbf42b35f3e00acaf4e3e86bff
Anatolii Saevskii <ansaev@gmail.com>
2020-01-10 13:42:55 +0300

    DEV-3950: delete key test

--- v2/client_test.go
+++ v2/client_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestClientInitWallet(t *testing.T) {
+func TestClient_InitWallet(t *testing.T) {
 	// parse config
 	options, err := ParseConfigFile("./tonlib.config.json.example")
 	if err != nil {
@@ -27,7 +27,7 @@ func TestClientInitWallet(t *testing.T) {
 }
 
 
-func TestClientCreateNewKey(t *testing.T) {
+func TestClient_CreateNewKey(t *testing.T) {
 	// parse config
 	options, err := ParseConfigFile("./tonlib.config.json.example")
 	if err != nil {
@@ -58,4 +58,44 @@ func TestClientCreateNewKey(t *testing.T) {
 		t.Fatal("Ton create key for init wallet error", err)
 	}
 	fmt.Println(fmt.Sprintf("pKey: %#v", pKey))
+}
+
+func TestClient_Deletekey(t *testing.T) {
+	// parse config
+	options, err := ParseConfigFile("./tonlib.config.json.example")
+	if err != nil {
+		t.Fatal("parse config error. ", err)
+	}
+
+	// make req
+	req := TonInitRequest{
+		"init",
+		*options,
+	}
+
+	// create client
+	cln, err := NewClient(&req, Config{})
+	if err != nil {
+		t.Fatal("Init client error. ", err)
+	}
+	defer cln.Destroy()
+
+	// prepare data
+	loc := SecureBytes("dsfsdfdsf")
+	mem := SecureBytes("1w1w1w1w1")
+	seed := SecureBytes("")
+
+	// create new key
+	pKey, err := cln.Createnewkey(&loc, &mem, &seed)
+	if err != nil {
+		t.Fatal("Ton create key for init wallet error", err)
+	}
+	fmt.Println(fmt.Sprintf("pKey: %#v", pKey))
+
+	// delete new key
+	ok, err :=  cln.Deletekey(pKey)
+	if err != nil {
+		t.Fatal("failed to delete new key", err)
+	}
+	fmt.Println(fmt.Sprintf("Ok: %#v, err: %v. ", ok, err))
 }
