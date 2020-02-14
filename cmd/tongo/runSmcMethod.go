6b9cf6ab31c54ca22789617275c24162776a7615
Anatolii Saevskii <ansaev@gmail.com>
2020-02-14 17:45:50 +0300

    runmethod: added args

--- cmd/tongo/runSmcMethod.go
+++ cmd/tongo/runSmcMethod.go
@@ -11,13 +11,15 @@ import (
 
 var runSmcMethodCmd = &cobra.Command{
 	Use:   "runSmcMethod",
-	Short: "Get raw account state",
-	Long: `Get raw account state command. It contains two attributes:
+	Short: "Run smc method",
+	Long: `Run smc method. It contains thre or more attributes:
 - path2configfile. see tonlib.config.json.example
 - smc address % - in the beginig - required!
+- smc method name % - in the beginig - required!
+- extra decimal args - as many as you want
 `,
 	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 2 {
+		if len(args) < 3 {
 			return fmt.Errorf("you have to use more than two args for this commaond \n")
 		}
 		_, err := os.Stat(args[0])
@@ -26,11 +28,11 @@ var runSmcMethodCmd = &cobra.Command{
 		}
 
 		// check address
-		if len(args[1]) < 2{
+		if len(args[1]) < 2 {
 			fmt.Printf("failed to parse adress: %s/n", args[1])
 			os.Exit(1)
 		}
-		if args[1][0:1] != "%"{
+		if args[1][0:1] != "%" {
 			fmt.Printf("adress have to start with `%` but got: `%s` /n", args[1])
 			os.Exit(1)
 		}
@@ -46,25 +48,31 @@ func runSmcMethod(cmd *cobra.Command, args []string) {
 		os.Exit(0)
 	}
 
+	// parse address
 	// cut  % in the begining of address- escape symbol
 	address := args[1][1:]
+	methodName := args[2]
 
-	// parse address
+	// parse extra params
+	params := []tonlib.TvmStackEntry{}
+	for _, arg := range (args[3:]) {
+		params = append(params, tonlib.NewTvmNumberDecimal(arg))
+	}
+
 	// load adresss
 	smcInfo, err := tonClient.SmcLoad(*tonlib.NewAccountAddress(address))
-	fmt.Println("smcInfo: %#v, err: %v",smcInfo ,err)
-	if err != nil{
+	if err != nil {
 		log.Fatal("Failed to SmcLoad: ", err)
 	}
+	fmt.Printf("smcInfo: %#v \n", smcInfo)
 
 	// run smc
 	res, err := tonClient.SmcRunGetMethod(
 		smcInfo.Id,
-		tonlib.NewSmcMethodIdName("active_election_id"),
-		[]tonlib.TvmStackEntry{},
-		)
-	fmt.Println("run smc: %#v, err: %v",res ,err)
-	if err != nil{
+		tonlib.NewSmcMethodIdName(methodName),
+		params,
+	)
+	if err != nil {
 		log.Fatal("Failed to run smc method: ", err)
 	}
 	fmt.Printf("Got a result: SmcRunResult :%#v;. Errors: %v. \n", res, err)
