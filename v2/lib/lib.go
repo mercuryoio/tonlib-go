e242caadffa542b80bf6d69ce591ac71cdd8acee
Anatolii Saevskii <ansaev@gmail.com>
2020-02-28 13:32:35 +0300

    v2: fix not inlcuding linux darwin directories to v2

--- v2/lib/lib.go
+++ v2/lib/lib.go
@@ -1,6 +1,6 @@
 package linux
 
 import (
-	_ "github.com/mercuryoio/tonlib-go/lib/darwin"
-	_ "github.com/mercuryoio/tonlib-go/lib/linux"
+	_ "github.com/mercuryoio/tonlib-go/v2/lib/darwin"
+	_ "github.com/mercuryoio/tonlib-go/v2/lib/linux"
 )
